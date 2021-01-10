fc017a8d80ae78afb8f68354f01ec72fad15d540
Nabih Mochir <Nabih.mochir@um5r.ac.ma>
2021-01-11 00:36:06 +0100

    Create main.c
    first file ?

---
+++ main.c
@@ -0,0 +1,11 @@
+/*Hello Stranger! here you will find the main code for our Othello project made by
+Manal & Nabih, have fun! */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* code */
+    return 0;
+}
+
