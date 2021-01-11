818bf6a39925add7fcb2fce6d8f9171a400ba21f
Nabih Mochir <Nabih.mochir@um5r.ac.ma>
2021-01-11 22:48:56 +0100

    fuck it

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-/*Hello Stranger! here you will find the main code for our Othello project made by
+
+    /*Hello Stranger! here you will find the main code for our Othello project made by
 Manal & Nabih, have fun! */
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,17 +9,21 @@ int main()
 
 char etat;
 int re;
-/**
+/*par une saisie du mot recomencer*/
 printf("est ce que tu veux jouer entrer recommencer");
-scanf("%s",etat);
-re=strcmp(etat,"recommencer");****/
+scanf("%s",&etat);
+re=strcmp(etat,"recommencer");
 if(re==0){
  int i;
  printf("\t    A   B   C   D   E   F   G   H  ");
  for(i=0;i<8;i++){
   printf("\t  +---+---+---+---+---+---+---+---+\n");}
  printf("\t    A   B   C   D   E   F   G   H  \n ");}
-  
-/****caracteristiques*****/
-
-
+/*par la touche entrer comme symbole de recommencer**/
+scanf("%d",&re);
+if (re=13 )/**le code ascii de la touche entrer ***/{
+int i;
+ printf("\t    A   B   C   D   E   F   G   H  ");
+ for(i=0;i<8;i++){
+  printf("\t  +---+---+---+---+---+---+---+---+\n");}
+ printf("\t    A   B   C   D   E   F   G   H  \n ");}}
