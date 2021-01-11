9f67a30ce9bd410cd414ae7e2cf9b17d430c7a69
Nabih Mochir <Nabih.mochir@um5r.ac.ma>
2021-01-11 22:56:01 +0100

    oh god please

--- main.c
+++ main.c
@@ -3,27 +3,27 @@
 Manal & Nabih, have fun! */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
 
-char etat;
+char* etat;
 int re;
+int i;
 /*par une saisie du mot recomencer*/
-printf("est ce que tu veux jouer entrer recommencer");
+printf("est ce que tu veux jouer entrer recommencer\n");
 scanf("%s",&etat);
 re=strcmp(etat,"recommencer");
 if(re==0){
- int i;
- printf("\t    A   B   C   D   E   F   G   H  ");
- for(i=0;i<8;i++){
-  printf("\t  +---+---+---+---+---+---+---+---+\n");}
- printf("\t    A   B   C   D   E   F   G   H  \n ");}
+    printf("\t    A   B   C   D   E   F   G   H  ");
+    for(i=0;i<8;i++){
+        printf("\t  +---+---+---+---+---+---+---+---+\n");}
+    printf("\t    A   B   C   D   E   F   G   H  \n ");}
 /*par la touche entrer comme symbole de recommencer**/
 scanf("%d",&re);
 if (re=13 )/**le code ascii de la touche entrer ***/{
-int i;
- printf("\t    A   B   C   D   E   F   G   H  ");
- for(i=0;i<8;i++){
-  printf("\t  +---+---+---+---+---+---+---+---+\n");}
- printf("\t    A   B   C   D   E   F   G   H  \n ");}}
+    printf("\t    A   B   C   D   E   F   G   H  ");
+    for(i=0;i<8;i++){
+        printf("\t  +---+---+---+---+---+---+---+---+\n");}
+        printf("\t    A   B   C   D   E   F   G   H  \n ");}}
