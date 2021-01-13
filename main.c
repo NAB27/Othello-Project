362e6a97e4922bfb56272138fa56a3cbce09bfcf
Manal58 <76754456+Manal58@users.noreply.github.com>
2021-01-14 00:33:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,39 @@
-
-    /*Hello Stranger! here you will find the main code for our Othello project made by
+/*Hello Stranger! here you will find the main code for our Othello project made by
 Manal & Nabih, have fun! */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 int main()
-{
-
-char* etat;
-int re;
+{/*****la partie 1 et 2******/
+    /*****recommencer*****/
+printf("\t hello ! \n");
+printf("\t voulez vous recommencez tapez la touche r\n ");
+printf("\t voulez vous continuer votre jeu tapez la touche c\n");
+char etat;
 int i;
-/*par une saisie du mot recomencer*/
-printf("est ce que tu veux jouer entrer recommencer\n");
-scanf("%s",&etat);
-re=strcmp(etat,"recommencer");
-if(re==0){
-    printf("\t    A   B   C   D   E   F   G   H  ");
+scanf("%c",&etat);
+if (etat==114){
+    printf(" \t voila! votre espace :\n");
+    printf("\t[-A-,-B-,-C-,-D-,-E-,-F-,-G-,-H-]\n");
     for(i=0;i<8;i++){
-        printf("\t  +---+---+---+---+---+---+---+---+\n");}
-    printf("\t    A   B   C   D   E   F   G   H  \n ");}
-/*par la touche entrer comme symbole de recommencer**/
-scanf("%d",&re);
-if (re=13 )/**le code ascii de la touche entrer ***/{
-    printf("\t    A   B   C   D   E   F   G   H  ");
-    for(i=0;i<8;i++){
-        printf("\t  +---+---+---+---+---+---+---+---+\n");}
-        printf("\t    A   B   C   D   E   F   G   H  \n ");}}
-        /* hihihi*/
-        /*test without saving & fitching?*/
+<<<<<<< HEAD
+        printf("\t ---,---,---,---,---,---,---,---,\n ");
+    }printf("\t[-A-,-B-,-C-,-D-,-E-,-F-,-G-,-H-]\n");}
+/****les caracteristiques****/
+FILE*fichier=NULL;//creatio d'un fichier
+//infos
+char nom[20];
+int score;
+char mot_de_passe;
+//enregistrement
+printf("donner le nom du joueur qui veut jouer par le blanc\n");
+scanf("%s",&nom);
+score=0;
+fopen(fichier,"w");
+fwrite(&nom,sizeof(nom),1,fichier);
+fwrite(&score,sizeof(score),1,fichier);
+printf("donner le nom du joueur qui veut jouer par le noir\n");
+scanf("%s",&nom);
+score=0;
+fwrite(&nom,sizeof(nom),1,fichier);
+fwrite(&score,sizeof(score),1,fichier);
+fclose(fichier);}
