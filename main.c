07b0fcebaaa31408af6c42a1542ae428f2ce2b3d
Manal58 <76754456+Manal58@users.noreply.github.com>
2021-02-07 23:15:31 +0100

    Update main.c
    les parties executées de mon code

--- main.c
+++ main.c
@@ -1,133 +1,574 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*Hello Stranger! here you will find the main code for our Othello project made by
-Manal & Nabih, have fun! */
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{/*****recommencer*****/
-printf("\t hello ! \n");
-printf("\t voulez vous recommencez tapez la touche r\n ");
-printf("\t voulez vous continuer votre jeu tapez la touche c\n");
-char etat;
-int i;
-scanf("%c",&etat);
-if (etat==114){
-     printf("\t####****************************####\n");
-     printf("\t##     OTHELLO GAME             ##\n");
-     printf("\t####****************************####\n");
-     printf("\t[-A-,-B-,-C-,-D-,-E-,-F-,-G-,-H-]\n");
-     for(i=0;i<8;i++){
-        printf("\t ---,---,---,---,---,---,---,---,\n ");
-    }printf("\t[-A-,-B-,-C-,-D-,-E-,-F-,-G-,-H-]\n");}
-/****les caracteristiques****/
-FILE*fichier;//creation d'un fichier
+#include<stdlib.h>
+#include<conio.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<time.h>
+/*************prototype*************/
+int*chercher(FILE*Fichier);
+void*creation_joueur();
+
+
+
+
+/*#####livrable1#######*/
+int*chercher(FILE*Fichier){
+    char motFr[30], motR[30];
+    Fichier = fopen("projet_c.txt", "r");
+    if (!Fichier)
+         printf("\aERREUR: Impossible d'ouvrir le fichier\n");
+    printf("\tentrer votre nom slv\t");
+    scanf("%s",&motR);
+    while(fgets(motFr,30,Fichier) != NULL)
+    {if (strstr(motFr,motR) != NULL){
+          printf("||||joueur deja existe bienvenue autre fois||||\n");
+          return 1;}
+    }
+    fclose(Fichier);
+    printf("|||bienvenue nouveau joueur entrer vos infos(nom) pour l'enregistrement|||\n ");
+    return 0;}
+
+/****fonction qui cree les joueurs nouveaux pour les autres conserve meme score**/
+
+void*creation_joueur(){
+FILE*fichier;
+fichier=fopen("projet_c","w+"); //la recherche premierement
 //infos
 char nom[20];
 int score;
 char mot_de_passe;
-//enregistrement_nouvelle_partie
-printf("donner le nom du joueur qui veut jouer par le blanc\n");
-scanf("%s",&nom);
+if((chercher(fichier))==0){
+scanf("%s",nom);
 score=0;
-fichier=fopen("projet_c","wba");
-if(fichier==NULL){printf("erreur d'overture du fichier");
-                exit(EXIT_FAILURE);}
-fwrite(&nom,sizeof(nom),1,fichier);
-fwrite(&score,sizeof(score),1,fichier);
-printf("donner le nom du joueur qui veut jouer par le noir\n");
-scanf("%s",&nom);
+fprintf(fichier,"%s\n%d\n ",&nom,&score);}
+/*****le deuxieme***/
+if(chercher(fichier)==0){
+scanf("%s",nom);
 score=0;
-fwrite(&nom,sizeof(nom),1,fichier);
-fwrite(&score,sizeof(score),1,fichier);
-fclose(fichier);}
-/***les parties***/
-//chaque joueur a une liste des paties chainees 
-typedef struct partie_joueur{
-//numero de la partie
-int num_partie;
-int score;
-struct partie_joueur*suivant;}partie_joueur;
-//une liste contient le nom du joueur et sa liste des parties
-typedef struct parties{
-char nom[20];
-struct partie_joueur;
-struct parties*suivant
-}parties;
-/***ajouter une partie***/
-void*ajouter(parties*debut){
-    printf("donner le nom du joueur");
-    char nom[20];
-    scanf("%s",&nom);
-    parties*temp=debut;
-    while((temp->suivant)!==NULL){if (strcmp(nom,temp->nom)==0){partie_joueur*temp1;
-                                                              temp1=temp->(partie_joueur);
-                                                            while(temp1->suivant!=NULL){temp1=temp1->suivant;}
-                                                            partie_joueur*nouveau=(partie_joueur*)malloc(sizeof(partie_joueur));
-                                                            nouveau->score=0;
-                                                            nouveau->num_partie=(temp1->num_partie)+1;}
-  temp=temp->suivant;}
-    }
-/***continuer en une partie deja souvgardee**/
-void*continuer(parties*debut){
-printf("donner le nom du joueur");
-char nom[20];
-scanf("%s",&nom);
-printf("donner le numero de la partie");
-int num_partie;
-scanf("%d",&num_partie);
-parties*temp=debut;
-while(temp->suivant!=NULL){if (strcmp(nom,temp->nom)==0){partie_joueur*temp1;
-                                                          temp1=temp->(partie_joueur);
-                                                            while(temp1->suivant!=NULL){
-                                                                if (temp1->num_partie=num_partie){printf("%d",&temp1->score);
-                                                                                                  /**ajout d'une partie**/
-                                                                                                  ajouter(parties*debut);}}}}}
+fprintf(fichier,"%s\n%d\n ",&nom,&score);}}
 
-/**affichage des 10 meilleurs scores****/
-void*meill_scores(parties*debut){
-    int i,j, s=0;
-    int indice=0;
-    parties*temp=debut;
-    typedef struct meil_score{
-          int score_t;
-          char nom;}meil_score;
-    meil_score*list;
-    list=(meil_score*)malloc(20*sizeof(meil_score));
-    while(temp->suivant!=NULL){ char nom[20];
-                                nom=temp->nom;
-                                partie_joueur*temp1=temp->(partie_joueur);
-                               while(temp1->suivant!=NULL){s=s+temp1->score;//score de toute les partie jouees
-                                                            }
-                                                            meil_score*temp2;
-                                                            temp2=(meil_score*)malloc(sizeof(meil_score));
-                                                            temp2->score_t=s;
-                                                            stcrpy(temp2->nom,nom);
-                                                            list[indice]=temp2;
-                               temp=temp->suivant;
-                               indice=indice+1;
-    return list}
-    meil_score*l=meill_scores(parties*debut);
-/****tri du tableau***/
-for (i=0 ; i <indice+1; i++)
-  {for (j=0 ; j <indice; j++){
-      if ((l[j]->score)>(l[j+1]->score)) 
-      {meil_score*tmp =l[j];
-       l[j] =l[j+1];
-      l[j+1] =tmp;}}}
-/***meilleur score***/
-printf("*******les meilleurs scores******");
-for(i=0;i<indice+1;i++){
-             printf("%d\t",l[i]->score);
-             printf("%s\n",l[i]->nom);}}
-/******jeu*****/
-char board[8][8];
-int i,j;
-for(i=0;i<8;i++){
-    for(j=0;j<8;j++){board[i][j]="_";}}
-for(i=0;i<8;i++){
-    for(j=0;j<8;j++){printf("%d",board[i][j]);}
-    printf("\n");}
-board[3][3]=board[4][4]=b;
-board[3][4]=board[4][3]=n;/**suite**/
 
+/*########livrable2########*/
+void Display(char [8][8]);   //affichage du tableau
+void position(short x, short y)           //definition of gotoxy function//
+{
+ COORD pos ={x,y};  //la position
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+void Display(char Board[8][8])
+{
+	system("cls");  //supprimer le contenu d'ecran a chaque fois
+
+ int i,j;
+ printf("  +---+---+---+---+---+---+---+---+\n");
+ for(i=0;i<8;i++)
+ {
+  printf("%d",i+1);
+  for(j=0;j<8;j++){
+  printf(" |%c ",Board[i][j]);}
+ printf("\n  +---+---+---+---+---+---+---+---+\n");}
+ printf("   A   B   C   D   E   F   G   H  ");
+}
+/**####MAIN#####**/
+int main(){
+Intro:     //label
+
+system("cls");
+int i,j,W,Done,Melon=0;
+char A,B,pos[2],X,Y,Board[8][8];
+for(i=0;i<8;i++)
+{
+ for(j=0;j<8;j++)
+ Board[i][j]=' ';
+}
+printf("\t\t  ####****************************####\n");
+printf("\t\t  ##\t      OTHELLO GAME    \t    ##\n");
+printf("\t\t  ####****************************####\n");
+Sleep(3000);
+printf("Entrer votre symbole et symbole d'adversaire ");
+scanf("%c %c",&A,&B);
+if(A==B)
+{
+ printf("les symboles se ressemblent........");
+ Sleep(1000);
+ goto Intro;
+}
+char tICK = A;
+char Tick = B;
+char *Quote[3]={"Le jeu","commence","maintenant!!!"};
+for(i=0;i<3;i++)
+{
+ printf("\n\n \t\t\t%s",Quote[i]);
+ Sleep(500);
+}
+Sleep(1000);
+Board[3][3]=A; Board[3][4]=B;
+Board[4][3]=B; Board[4][4]=A;
+start:
+Display(Board);
+A = tICK;
+printf("(%c) votre tour........",A);
+Sleep(1000);
+printf("\nEntrer votre Position: ( tq A3,B4)");
+gets(pos);
+pos[0]=toupper(pos[0]); //la gestion de position par rapport au tableau
+if(pos[0]=='A')
+X = 0;
+else if(pos[0]=='B')
+X = 1;
+else if(pos[0]=='C')
+X = 2;
+else if(pos[0]=='D')
+X = 3;
+else if(pos[0]=='E')
+X = 4;
+else if(pos[0]=='F')
+X = 5;
+else if(pos[0]=='G')
+X = 6;
+else if(pos[0]=='H')
+X = 7;
+else
+{
+ printf("Entrer une autre fois...");
+ Sleep(500);
+ goto start;
+}
+if(pos[1]=='1')
+Y = 0;
+else if(pos[1]=='2')
+Y = 1;
+else if(pos[1]=='3')
+Y = 2;
+else if(pos[1]=='4')
+Y = 3;
+else if(pos[1]=='5')
+Y = 4;
+else if(pos[1]=='6')
+Y = 5;
+else if(pos[1]=='7')
+Y = 6;
+else if(pos[1]=='8')
+Y = 7;
+else
+{
+ printf("Entrer une autre fois...");
+ Sleep(500);
+ goto start;
+}
+if(Board[Y][X]!=' ')
+{
+ printf("Entrer une autre fois...");
+ Sleep(500);
+ goto start;
+}
+Board[Y][X]=A;
+B = Tick;
+//teste du cote gauche
+for(j=X-1;j>=0;j--)
+{
+ if(Board[Y][j]==B)
+ Board[Y][j]=A;
+ else
+ break;
+}
+if(Board[Y][j]!=A)
+{
+ for(j=X-1;j>=0;j--)
+ {
+  if(Board[Y][j]==A)
+  Board[Y][j]=B;
+  else
+  break;
+ }
+}
+// teste du cote droit
+for(j=X+1;j<8;j++)
+{
+ if(Board[Y][j]==B)
+ Board[Y][j]=A;
+ else
+ break;
+}
+if(Board[Y][j]!=A)
+{
+ for(j=X+1;j<8;j++)
+ {
+  if(Board[Y][j]==A)
+  Board[Y][j]=B;
+  else
+  break;
+ }
+}
+//teste de la pratie haute
+for(i=Y-1;i>=0;i--)
+{
+ if(Board[i][X]==B)
+ Board[i][X]=A;
+ else
+ break;
+}
+if(Board[i][X]!=A)
+{
+ for(i=Y-1;i>=0;i--)
+ {
+  if(Board[i][X]==A)
+  Board[i][X]=B;
+  else
+  break;
+ }
+}
+// teste de la partie basse
+for(i=Y+1;i<8;i++)
+{
+ if(Board[i][X]==B)
+ Board[i][X]=A;
+ else
+ break;
+}
+if(Board[i][X]!=A)
+{
+ for(i=Y+1;i<8;i++)
+ {
+  if(Board[i][X]==A)
+  Board[i][X]=B;
+  else
+  break;
+ }
+}
+// teste de la partie droite de la diagonale
+i = Y-1; j = X-1;
+while(i>=0 && j>=0)
+{
+ if(Board[i][j]==B)
+ Board[i][j]=A;
+ else
+ break;
+ i--; j--;
+}
+if(Board[i][j]!=A)
+{
+ i = Y-1; j = X-1;
+ while(i>=0 && j>=0)
+ {
+  if(Board[i][j]==A)
+  Board[i][j]=B;
+  else
+  break;
+  i--; j--;
+ }
+}
+// teste de la partie gauche de la diagonale
+i = Y-1; j = X+1;
+while(i>=0 && j<8)
+{
+ if(Board[i][j]==B)
+ Board[i][j]=A;
+ else
+ break;
+ i--; j++;
+}
+if(Board[i][j]!=A)
+{
+ i = Y-1; j = X+1;
+ while(i>=0 && j<8)
+ {
+  if(Board[i][j]==A)
+  Board[i][j]=B;
+  else
+  break;
+  i--; j++;
+ }
+}
+//teste de la partie gauche de la contre diagonale
+i = Y+1; j = X-1;
+while(i<8 && j>=0)
+{
+ if(Board[i][j]==B)
+ Board[i][j]=A;
+ else
+ break;
+ i++; j--;
+}
+if(Board[i][j]!=A)
+{
+ i = Y+1; j = X-1;
+ while(i<8 && j>=0)
+ {
+  if(Board[i][j]==A)
+  Board[i][j]=B;
+  else
+  break;
+  i++; j--;
+ }
+}
+// teste de la patie droite du contre diagonale
+i = Y+1; j = X+1;
+while(i<8 && j<8)
+{
+ if(Board[i][j]==B)
+ Board[i][j]=A;
+ else
+ break;
+ i++; j++;
+}
+if(Board[i][j]!=A)
+{
+ i = Y+1; j = X+1;
+ while(i<8 && j<8)
+ {
+  if(Board[i][j]==A)
+  Board[i][j]=B;
+  else
+  break;
+  i++; j++;
+ }
+}
+Display(Board);
+Sleep(500);
+Done = 1;
+for(i=0;i<8;i++)
+{
+ for(j=0;j<8;j++)
+ {
+  if(Board[i][j]==' ')
+  Done = 0;
+ }
+}
+if(Done==1)
+goto end;
+/*                                */
+printf("(%c) votre Tour........",B);
+printf("\nEntrer votre Position: ( tq A3,B4)");
+Sleep(1000);
+again:
+X = rand()%8;
+Y = rand()%8;
+W = 0;
+if(Board[Y][X]!=' ')
+goto again;
+ if(Board[Y+1][X]==A)
+ W++;
+ if(Board[Y][X+1]==A)
+ W++;
+ if(Board[Y+1][X+1]==A)
+ W++;
+ if(Board[Y-1][X]==A)
+ W++;
+ if(Board[Y][X-1]==A)
+ W++;
+ if(Board[Y-1][X-1]==A)
+ W++;
+ if(Board[Y+1][X-1]==A)
+ W++;
+ if(Board[Y-1][X+1]==A)
+ W++;
+if(W==0 && Melon<15)
+goto again;
+Board[Y][X] = B;
+Melon++;
+//gauche
+for(j=X-1;j>=0;j--)
+{
+ if(Board[Y][j]==A)
+ Board[Y][j]=B;
+ else
+ break;
+}
+if(Board[Y][j]!=B)
+{
+ for(j=X-1;j>=0;j--)
+ {
+  if(Board[Y][j]==B)
+  Board[Y][j]=A;
+  else
+  break;
+ }
+}
+// a droite
+for(j=X+1;j<8;j++)
+{
+ if(Board[Y][j]==A)
+ Board[Y][j]=B;
+ else
+ break;
+}
+if(Board[Y][j]!=B)
+{
+ for(j=X+1;j<8;j++)
+ {
+  if(Board[Y][j]==B)
+  Board[Y][j]=A;
+  else
+  break;
+ }
+}
+// haut
+for(i=Y-1;i>=0;i--)
+{
+ if(Board[i][X]==A)
+ Board[i][X]=B;
+ else
+ break;
+}
+if(Board[i][X]!=B)
+{
+ for(i=Y-1;i>=0;i--)
+ {
+  if(Board[i][X]==B)
+  Board[i][X]=A;
+  else
+  break;
+ }
+}
+// bas
+for(i=Y+1;i<8;i++)
+{
+ if(Board[i][X]==A)
+ Board[i][X]=B;
+ else
+ break;
+}
+if(Board[i][X]!=B)
+{
+ for(i=Y+1;i<8;i++)
+ {
+  if(Board[i][X]==B)
+  Board[i][X]=A;
+  else
+  break;
+ }
+}
+// gauche diagonale
+i = Y-1; j = X-1;
+while(i>=0 && j>=0)
+{
+ if(Board[i][j]==A)
+ Board[i][j]=B;
+ else
+ break;
+ i--; j--;
+}
+if(Board[i][j]!=B)
+{
+ i = Y-1; j = X-1;
+ while(i>=0 && j>=0)
+ {
+  if(Board[i][j]==B)
+  Board[i][j]=A;
+  else
+  break;
+  i--; j--;
+ }
+}
+// droite diagonale
+i = Y-1; j = X+1;
+while(i>=0 && j<8)
+{
+ if(Board[i][j]==A)
+ Board[i][j]=B;
+ else
+ break;
+ i--; j++;
+}
+if(Board[i][j]!=B)
+{
+ i = Y-1; j = X+1;
+ while(i>=0 && j<8)
+ {
+  if(Board[i][j]==B)
+  Board[i][j]=A;
+  else
+  break;
+  i--; j++;
+ }
+}
+// droite contre diagonale
+i = Y+1; j = X-1;
+while(i<8 && j>=0)
+{
+ if(Board[i][j]==A)
+ Board[i][j]=B;
+ else
+ break;
+ i++; j--;
+}
+if(Board[i][j]!=B)
+{
+ i = Y+1; j = X-1;
+ while(i<8 && j>=0)
+ {
+  if(Board[i][j]==B)
+  Board[i][j]=A;
+  else
+  break;
+  i++; j--;
+ }
+}
+// gauche contre diagonale
+i = Y; j = X;
+while(i<8 && j<8)
+{
+ if(Board[i][j]==A)
+ Board[i][j]=B;
+ else
+ break;
+ i++; j++;
+}
+if(Board[i][j]!=B)
+{
+ i = Y; j = X;
+ while(i<8 && j<8)
+ {
+  if(Board[i][j]==B)
+  Board[i][j]=A;
+  else
+  break;
+  i++; j++;
+ }
+}
+Display(Board);
+Sleep(500);
+Done = 1;
+for(i=0;i<8;i++)
+{
+ for(j=0;j<8;j++)
+ {
+  if(Board[i][j]==' ')
+  Done = 0;
+ }
+}
+if(Done==1)
+goto end;
+goto start;
+end:
+Sleep(1000);
+int Pla=0,Opp=0;
+for(i=0;i<8;i++)
+{
+ for(j=0;j<8;j++)
+ {
+  if(Board[i][j]==A)
+  Pla++;
+  if(Board[i][j]==B)
+  Opp++;
+ }
+}
+system("cls");
+if(Pla>Opp)
+{
+ position(30,10);
+ printf("vous avez ruessi !!!!!!");
+}
+if(Pla<Opp)
+{
+ position(30,10);
+ printf("votre adversaire a ruessi !!!!!!");
+}
+if(Pla==Opp)
+{
+ position(30,10);
+ printf("egalite !!!!!!");
+}
+getch();
+}
